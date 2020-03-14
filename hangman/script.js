676b0a843b912ce73668c84fea6e427a65f88115
Jungwon Oh <jungwon.oh87@gmail.com>
2020-03-14 14:34:06 -0700

    play again button works

--- hangman/script.js
+++ hangman/script.js
@@ -6,12 +6,13 @@ const notification_container = document.getElementById(
   "notification_container"
 );
 const wrong_letters_element = document.getElementById("wrong_letters");
-const figure_parts = document.querySelectorAll("figure_part");
-const correct_letters = [];
-const wrong_letters = [];
+const figure_parts = document.querySelectorAll(".figure_part");
+const play_btn = document.getElementById("play_btn");
+let correct_letters = [];
+let wrong_letters = [];
 let selected_word = "";
+const api_key = config.x_rapidapi_key;
 
-let newData = "test";
 const url = "https://wordsapiv1.p.rapidapi.com/words/?random=true";
 
 async function getAPI(url) {
@@ -19,7 +20,7 @@ async function getAPI(url) {
     method: "GET",
     headers: {
       "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-      "x-rapidapi-key": "eb70fdab9fmshcd990b979590c50p1a4acdjsnc2feb87bda92"
+      "x-rapidapi-key": api_key
     }
   });
   return response.json();
@@ -30,9 +31,6 @@ function displayWord(selected_word) {
     .split("")
     .map(
       letter =>
-        // PUSH EACH LETTER TO CORRECT_LETTERS VARIABLE FOR TEST
-        // correct_letters.push(letter);
-
         // CREATE BLANKS FOR # OF WORD AND CHECK EACH LETTER
         `<span class="letter">
         ${correct_letters.includes(letter) ? letter : ""}
@@ -40,66 +38,60 @@ function displayWord(selected_word) {
     )
     .join("")}`;
 
-  // ANSWER IN ONE SINGLE LINE
-  const answer_display_wo_newline = answer_display.innerText.replace(/\n/g, "");
-  console.log("answer_w/o_newline: ", answer_display_wo_newline);
-
-  if (answer_display_wo_newline === selected_word) {
-    final_message.innerText = "Congrats, You won!";
-    popup_container.style.display = "flex";
-  }
+  displayPopup();
 }
+function startGame() {
+  // RESET THE GAME
+  correct_letters = [];
+  wrong_letters = [];
+  popup_container.style.display = "none";
 
-getAPI(url).then(data => {
-  // displayWord(data);
-  // TAKE THE FIRST WORD ONLY
-  const words = data.word.split(" ");
-  selected_word = words[0];
-  console.log("selected word: ", selected_word);
+  getAPI(url).then(data => {
+    // TAKE THE FIRST WORD ONLY IF THERE ARE TWO
+    const words = data.word.split(" ");
+    selected_word = words[0];
+    console.log("selected word: ", selected_word);
 
-  displayWord(selected_word);
+    displayWord(selected_word);
 
-  // DISPLAY HINT
-  getHint(data);
+    // DISPLAY HINT
+    getHint(data);
 
-  // POP UP
-  // display_popup();
+    window.addEventListener("keydown", e => {
+      if ((e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode === 189) {
+        // GET THE KEY INPUT
+        const letter = e.key;
+        // CHECK IF THE LETTER IS IN THE SELECTED WORD
+        if (selected_word.includes(letter)) {
+          // CHECK IF THE LETTER IS ALREADY IN CORRECT LETTERS. SHOULD NOT!
+          if (!correct_letters.includes(letter)) {
+            // THEN PUSH THE LETTER TO CORRECT LETTERS
+            correct_letters.push(letter);
 
-  window.addEventListener("keydown", e => {
-    // console.log(e.keyCode);
-    if ((e.keyCode >= 65 && e.keyCode <= 90) || e.keyCode === 189) {
-      // GET THE KEY INPUT
-      const letter = e.key;
-      // CHECK IF THE LETTER IS IN THE SELECTED WORD
-      if (selected_word.includes(letter)) {
-        // CHECK IF THE LETTER IS ALREADY IN CORRECT LETTERS. SHOULD NOT!
-        if (!correct_letters.includes(letter)) {
-          // THEN PUSH THE LETTER TO CORRECT LETTERS
-          correct_letters.push(letter);
-
-          // UPDATE DISPLAY
-          displayWord(selected_word);
+            // UPDATE DISPLAY
+            displayWord(selected_word);
+          } else {
+            // WHEN WE ALREADY HAVE THE LETTER IN CORRECT LETTERS
+            showNotification();
+          }
         } else {
-          // WHEN WE ALREADY HAVE THE LETTER IN CORRECT LETTERS
-          showNotification();
-        }
-      } else {
-        // WRONG LETTERS
-        // CHECK IF WE ALREADY HAVE THE LETTER IN WRONG LETTERS. SHOULD NOT!
-        if (!wrong_letters.includes(letter)) {
-          // INSERT THE LETTER IN WRONG LETTERS
-          wrong_letters.push(letter);
-          console.log("wrong_letters: ", wrong_letters);
-          // UPDATE WRONG LETTER
-          updateWrongLetterEl();
-        } else {
-          // WHEN WE ALREADY HAVE THE LETTER IN WRONG LETTERS
-          showNotification();
+          // WRONG LETTERS
+          // CHECK IF WE ALREADY HAVE THE LETTER IN WRONG LETTERS. SHOULD NOT!
+          if (!wrong_letters.includes(letter)) {
+            // INSERT THE LETTER IN WRONG LETTERS
+            wrong_letters.push(letter);
+            console.log("wrong_letters: ", wrong_letters);
+            // UPDATE WRONG LETTER
+            updateWrongLetterEl();
+          } else {
+            // WHEN WE ALREADY HAVE THE LETTER IN WRONG LETTERS
+            showNotification();
+          }
         }
       }
-    }
+    });
   });
-});
+}
 
 function updateWrongLetterEl() {
   // UPDATE WRONG LETTER ELEMENT
@@ -109,9 +101,20 @@ function updateWrongLetterEl() {
     return `<span>${letter}</span>`;
   })}`;
 
-  // ADD A FIGURE
-  figure_part.forEach((part, index) => {});
+  // ADD A FIGURE PART
+  figure_parts.forEach((part, index) => {
+    const error_num = wrong_letters.length;
+    if (error_num > index) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
   // FIGURE IS COMPLETED AND GAME OVER
+  if (wrong_letters.length >= 6) {
+    final_message.innerText = "You have failed:(";
+    popup_container.style.display = "flex";
+  }
 }
 
 function showNotification() {
@@ -124,7 +127,7 @@ function showNotification() {
   }, 2000);
 }
 
-function display_popup() {
+function displayPopup() {
   // ANSWER IN ONE SINGLE LINE
   const answer_display_wo_newline = answer_display.innerText.replace(/\n/g, "");
   console.log("answer_w/o_newline: ", answer_display_wo_newline);
@@ -140,3 +143,9 @@ function getHint(data) {
     data.results == null ? "no hint available" : data.results[0].definition
   }`;
 }
+
+startGame();
+
+play_btn.addEventListener("click", () => {
+  startGame();
+});
