32c8600c8a58739eee6755e56b245d57c824f18a
Jungwon Oh <jungwon.oh87@gmail.com>
2020-03-22 22:18:43 -0700

    meal finder project - fetched mealDB api, display images based on search keyword, and hover effect.

--- meal_finder/script.js
+++ meal_finder/script.js
@@ -1,11 +1,35 @@
 const search_button = document.getElementById("search_button");
 const search_el = document.getElementById("search");
-const search_word = search_el.value;
+// const search_word = search_el.value; // find out why it doesn't work
+const result_heading = document.getElementById("result_heading");
+const meals_el = document.getElementById("meals");
 
 function searchMeal(e) {
   e.preventDefault();
-  //   fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-  console.log("search word: ", search_word);
+  const search_word = search_el.value;
+
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search_word}`)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      if (data.meals === null) {
+        result_heading.innerHTML = `<p>Sorry, we can't find any result for ${search_word}</p>`;
+      } else {
+        result_heading.innerHTML = `<p>We have some results for '${search_word}'</p>`;
+        meals_el.innerHTML = data.meals
+          .map(meal => {
+            return `<div class="meal">
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"></img>
+            <div class="meal_info" data-mealID="${meal.idMeal}">
+            <h3>${meal.strMeal}</h3>
+            </div>
+          </div>`;
+          })
+          .join("");
+      }
+      console.log(data);
+    });
 }
 
 search_button.addEventListener("click", searchMeal);
