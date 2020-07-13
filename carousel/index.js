75c82ab0ea1644784b13d9909b0dab727aa1da24
Jungwon Oh <jungwon.oh87@gmail.com>
2020-07-12 22:33:04 -0700

    carousel project - slide animation added

--- carousel/index.js
+++ carousel/index.js
@@ -1,5 +1,46 @@
-const slide_index = 1;
+const prev_nav = document.getElementById("prev_nav");
+const next_nav = document.getElementById("next_nav");
+
+let slide_index = 0;
 
 const currentSlide = (num) => {
-  console.log("num check: ", num);
+  slide_index = num;
+  showSlides(num);
 };
+
+const showSlides = (num) => {
+  const slides = document.getElementsByClassName("mySlides");
+  const dots = document.getElementsByClassName("dot");
+
+  if (num >= slides.length) {
+    slide_index = 0;
+  }
+  if (num < 0) {
+    slide_index = slides.length - 1;
+  }
+
+  // reset the slides to be invisible
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // reset dots
+  for (let i = 0; i < dots.length; i++) {
+    // check this again
+    dots[i].className = dots[i].className.replace("active", "");
+  }
+
+  slides[slide_index].style.display = "block";
+  dots[slide_index].classList.add("active");
+};
+
+showSlides(slide_index);
+prev_nav.addEventListener("click", () => {
+  slide_index--;
+  showSlides(slide_index);
+});
+
+next_nav.addEventListener("click", () => {
+  slide_index++;
+  showSlides(slide_index);
+});
